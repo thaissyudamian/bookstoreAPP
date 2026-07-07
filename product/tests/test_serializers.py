782457d5e85b91150e99a661aae3b4b6f5b29de5
thaissy <thaissy@thaissy-LINUX.com>
2026-07-07 14:41:40 -0300

    Add teste de category

--- product/tests/test_serializers.py
+++ product/tests/test_serializers.py
@@ -1,5 +1,5 @@
 import pytest
-from product.serializers import ProductSerializer
+from product.serializers import ProductSerializer, CategorySerializer
 from product.tests.factories import CategoryFactory, ProductFactory
 
 @pytest.mark.django_db
@@ -17,3 +17,21 @@ def test_product_serializer_retorna_campos():
     assert data["title"] == "Django REST"
     assert len(data["category"]) == 1
     assert data["category"][0]["title"] == "Livros"
+
+@pytest.mark.django_db
+def test_category_serializer_retorna_campos():
+    category = CategoryFactory(
+    title="Livros",
+    slug="livros",
+    description="Categoria de livros",
+    active=True,
+)
+
+    serializer = CategorySerializer(instance=category)
+    data = serializer.data
+
+    assert data["id"] == category.id
+    assert data["title"] == "Livros"
+    assert data["slug"] == "livros"
+    assert data["description"] == "Categoria de livros"
+    assert data["active"] is True
